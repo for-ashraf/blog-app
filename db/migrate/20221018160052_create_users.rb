415ef3eb3b91a67960e722dcb16b7e4bd9b30c76
Muhammad Ashraf <forazkar@gmail.com>
2022-10-18 23:25:49 +0500

    Updated Migrations
    - Updated migrations
    - Updated Schema

--- db/migrate/20221018160052_create_users.rb
+++ db/migrate/20221018160052_create_users.rb
@@ -2,10 +2,9 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :name
-      t.string :photo
-      t.string :bio
-      t.integer :postscounter
-
+      t.text :photo
+      t.text :bio
+      t.integer :posts_counter
       t.timestamps
     end
   end
