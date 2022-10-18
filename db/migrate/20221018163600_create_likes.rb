f6e7ddad395493a4959c590756c0a3804b9b42e6
Muhammad Ashraf <forazkar@gmail.com>
2022-10-18 23:43:43 +0500

    Fixed linters

--- db/migrate/20221018163600_create_likes.rb
+++ db/migrate/20221018163600_create_likes.rb
@@ -1,9 +1,8 @@
+# frozen_string_literal: true
+
 class CreateLikes < ActiveRecord::Migration[7.0]
   def change
-    create_table :likes do |t|
-
-      t.timestamps
-    end
+    create_table :likes, &:timestamps
     add_reference :likes, :users, null: false, foreign_key: true
     add_reference :likes, :posts, null: false, foreign_key: true
   end
