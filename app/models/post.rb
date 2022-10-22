823632d0da5efe4054091d69c17fc6eab04c452d
Muhammad Ashraf <forazkar@gmail.com>
2022-10-22 14:43:32 +0500

    Fixed Linter Errors
    Fixed Linter errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,7 +8,7 @@ class Post < ApplicationRecord
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   after_save :update_posts_counter_per_user
-  
+
   def five_most_recent_comments_per_post
     comments.last(5)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
 
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :name, presence: true
-  
+
   def three_recent_posts_per_user
     posts.last(3)
   end
