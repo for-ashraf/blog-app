23a8934c07ff8e789a76d97d21e51d9ac73ce7c0
Muhammad Ashraf <forazkar@gmail.com>
2022-10-25 23:35:58 +0500

    Update users_spec.rb
    - Incorporated a few test cases

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,8 +1,20 @@
-RSpec.describe "Users", type: :request do
-    describe "request list of all users" do
-      user = User.create(name: “Test user”)
-      get users_path
-      expect(response).to be_successful
-      expect(response.body).to include(“Test user”)
+require 'rails_helper'
+
+RSpec.describe 'Users controller', type: :request do
+  context 'testing users home route' do
+    it 'Should rennder users index page' do
+      get '/'
+      expect(response).to render_template('index')
+    end
+
+    it 'Rendered page should include "Listing Users"' do
+      get '/'
+      expect(response.body).to include('Listing Users')
+    end
+
+    it 'http status to be OK' do
+      get '/'
+      expect(response).to have_http_status(:ok)
     end
   end
+end
