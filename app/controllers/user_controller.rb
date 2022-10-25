e107e4bc15c301c066ff34c69b8a858b15ce8337
Muhammad Ashraf <forazkar@gmail.com>
2022-10-25 11:09:43 +0500

    Controllers
    Defined User and Post Controllers

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,14 +1,9 @@
-class UserController < ApplicationController
-    def index
-        @users = User.all
-      end
-    
-      def show
-        @user = User.find(params[:id])
-        @post = Post.where(user: @user.id)
-      end
-    
-      def new
-        @user = User.new
-      end
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    ; 
+  end
 end
