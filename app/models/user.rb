81a0990be0ec2d37464751edf654caade53a07aa
Muhammad Ashraf <forazkar@gmail.com>
2022-10-27 06:39:59 +0500

    Fixing Linters
    - Fixing Linters
    - Setting up small bugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,15 @@ class User < ApplicationRecord
 
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :name, presence: true
+  after_initialize :init
 
   def three_recent_posts_per_user
     posts.last(3)
+  end
+
+  private
+
+  def init
+    self.posts_counter ||= 0
   end
 end
