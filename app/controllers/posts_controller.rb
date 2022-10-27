2d7f1adc805d4ac9fae03e248e4b11ca1e3214bc
Muhammad Ashraf <forazkar@gmail.com>
2022-10-27 06:15:22 +0500

    Update posts_controller.rb
    To find individual post record

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,10 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @user = User.find(params[:user_id])
+    @posts = User.find(params[:user_id]).posts
   end
 
-  def show; end
+  def show
+    @post = Post.find(params[:id])
+  end
 end
