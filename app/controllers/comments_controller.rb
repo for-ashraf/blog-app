ad490bfeaef8548252b7902967a8c3aab82bec3e
Muhammad Ashraf <forazkar@gmail.com>
2022-10-28 12:00:02 +0500

    Fixing Linters

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,21 +1,21 @@
 class CommentsController < ApplicationController
-    def new
-      post_id = params[:id]
-      comment = Comment.new
-      respond_to do |format|
-        format.html { render :new_comment, locals: { comment: comment, post_id: post_id } }
-      end
-    end
-  
-    def create
-      post = Post.find(params[:id])
-      comment = Comment.new(text: params[:comment][:text], post: post, user: current_user)
-      if comment.save
-        flash[:success] = 'Comment saved successfully'
-        redirect_to "/users/:user_id/posts/#{post[:id]}"
-      else
-        flash.now[:error] = 'Error: Comment could not be saved'
-        render inline: '<p>Error</p>'
-      end
+  def new
+    post_id = params[:id]
+    comment = Comment.new
+    respond_to do |format|
+      format.html { render :new_comment, locals: { comment:, post_id: } }
     end
   end
+
+  def create
+    post = Post.find(params[:id])
+    comment = Comment.new(text: params[:comment][:text], post:, user: current_user)
+    if comment.save
+      flash[:success] = 'Comment saved successfully'
+      redirect_to "/users/:user_id/posts/#{post[:id]}"
+    else
+      flash.now[:error] = 'Error: Comment could not be saved'
+      render inline: '<p>Error</p>'
+    end
+  end
+end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,14 +12,13 @@ class PostsController < ApplicationController
   def new
     post = Post.new
     respond_to do |format|
-      format.html { render :new_post, locals: { post: post } }
+      format.html { render :new_post, locals: { post: } }
     end
   end
 
   def create
-    
     post = Post.new(title: params[:post][:title], text: params[:post][:text], user: current_user)
-    
+
     if post.save
       flash[:success] = 'Post saved successfully'
       redirect_to "/users/#{current_user.id}/posts"
@@ -31,7 +30,7 @@ class PostsController < ApplicationController
 
   def like
     post = Post.find(params[:id])
-    Like.create(post: post, user: current_user)
+    Like.create(post:, user: current_user)
     redirect_to "/users/:user_id/posts/#{post[:id]}"
   end
 end
