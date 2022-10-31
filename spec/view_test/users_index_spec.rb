99951b21741e678be5216263d57f323265ff4b16
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-10-31 21:22:36 +0300

    add one test

--- spec/view_test/users_index_spec.rb
+++ spec/view_test/users_index_spec.rb
@@ -1,4 +1,16 @@
 require 'rails_helper'
 RSpec.describe 'user_show', type: :feature do
-    @id
+   before(:each) do
+    @user = User.create(
+        name: 'muhammed ashraf',
+        photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+        bio: 'full_stack programmer'
+        posts_counter: 0
+    )
+    visit root_path
+   end
+
+   it 'show the name of the user' do 
+    expect(page).to have_content("#{@user.name}")
+   end
 end
